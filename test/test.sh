47bdb62116abcfe8f1506a6415aaada5af7c0d8f
Raymond Barbiero <raymond.barbiero.dev@gmail.com>
2015-05-02 02:00:50 -0700

    use upstream rsync code. remove omnibus from tests - coreos doesnt support omnibus. add user-data.yml for coreos test harness

--- test/test.sh
+++ test/test.sh
@@ -1,14 +1,13 @@
 # if ! bundle exec vagrant box list | grep digital_ocean 1>/dev/null; then
 #     bundle exec vagrant box add digital_ocean box/digital_ocean.box
 # fi
+set -e ; set -u ; set -o pipefail
 
 cd test
-
 bundle exec vagrant up --provider=digital_ocean
-bundle exec vagrant up
 bundle exec vagrant provision
 bundle exec vagrant rebuild
 bundle exec vagrant halt
-bundle exec vagrant destroy
+bundle exec vagrant destroy -f
 
 cd ..
